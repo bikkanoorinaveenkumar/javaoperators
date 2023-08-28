cea8b93dc2ed92ab226e9170155a0ead1a9c1de7
bikkanoorinaveenkumar <naveenkumarbikkanoori@gmail.com>
2023-08-29 04:35:27 +0530

    operators

---
+++ Unary.java
@@ -0,0 +1,11 @@
+class Unary
+{
+public static void main(String[] args)
+{
+int x=9,y,m=6,n;
+y=x++;//Post Increment
+System.out.println("x:"+x+"y:"+y);
+n=++m;//Pre Increment
+System.out.println("m:"+m+"n:"+n);
+}
+}
---
+++ Unaryd.java
@@ -0,0 +1,11 @@
+class Unaryd
+{
+public static void main(String[] args)
+{
+int x=9,y,m=8,n;
+y=x--;//Post decreament step1:y=x step2:x=X-1
+System.out.println("x:"+x+"y:"+y);
+n=--m;//Pre decrement step1:m=m-1 step2:n=m
+System.out.println("m:"+m+"n:"+n);
+}
+}
